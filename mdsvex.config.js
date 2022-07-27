e028ed3fa05d27be2918aa58e7a07be37a1cf405
Alexander Daichendt <alexander.daichendt@tum.de>
2022-07-27 07:41:29 +0200

    feat: add react gfm

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,17 +1,18 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import remarkGFM from 'remark-gfm';
 
 const config = defineConfig({
 	layout: {
-		blog: './src/layouts/blog.svelte'
+		blog: './src/layouts/blog.svelte',
 	},
 	extensions: ['.svelte.md', '.md', '.svx'],
 
 	smartypants: {
-		dashes: 'oldschool'
+		dashes: 'oldschool',
 	},
 
-	remarkPlugins: [],
-	rehypePlugins: []
+	remarkPlugins: [remarkGFM],
+	rehypePlugins: [],
 });
 
 export default config;
