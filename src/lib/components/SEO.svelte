6157750cca1b7329731a63d09ea10ec107daa2f5
Alexander Daichendt <alexander.daichendt@tum.de>
2022-08-22 17:01:33 +0200

    fix: seo errors

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -3,23 +3,23 @@
 
 	export let title = "Alex Daichendt's website";
 	export let keywords: string[] = [];
-	export let description: string;
+	export let description: string = '';
 	let seo = $page.data?.seo;
 
 	if (seo) {
 		title = seo.title ? `${seo.title} - Alex Daichendt` : "Alex Daichendt's website";
 		description = seo.description;
-		keywords = seo.keywords;
+		keywords = seo.keywords || [];
 	}
 </script>
 
 <svelte:head
 	><title>{title}</title>
-	{#if description}
+	{#if description.length > 0}
 		<meta name="description" content={description} />
 	{/if}
 	<meta name="author" content="Alexander Daichendt" />
-	{#if keywords}
+	{#if keywords.length > 0}
 		<meta name="keywords" content={keywords.join(',')} />
 	{/if}
 </svelte:head>
